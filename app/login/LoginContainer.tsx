61693492b865a29ec29c4b4450780c0de043ab62
Gaurang Patel <meetgaurang@gmail.com>
2025-09-27 10:08:02 +1000

    Renamed schema file

--- app/login/LoginContainer.tsx
+++ app/login/LoginContainer.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabaseClient } from "~/supabase";
-import type { LoginFormValues } from "./loginFormSchema";
+import { type LoginFormValues } from "./loginFormSchema";
 import { Login } from "./login";
 
 export function LoginContainer() {
